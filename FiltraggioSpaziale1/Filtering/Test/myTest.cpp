31f79645ce011a35280479e74943c184531485e1
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2024-01-03 19:03:19 +0100

    todo

--- FiltraggioSpaziale1/Filtering/Test/myTest.cpp
+++ FiltraggioSpaziale1/Filtering/Test/myTest.cpp
@@ -4,7 +4,22 @@
 using namespace cv;
 using namespace std;
 
-Mat
+Mat correlation1D(const Mat& imageInput) {
+    Mat imageOutput = Mat::zeros(imageInput.size(), imageInput.type());
+
+    for(int x = 0; x < imageInput.rows; ++x) {
+        for(int y = 1; y < imageInput.cols - 1; ++y) {
+            float sum = 0.0;
+            sum += imageInput.at<float>(x, y-1)*(float)(1.0f/3.0f);
+            sum += imageInput.at<float>(x, y)*(float)(1.0f/3.0f);
+            sum += imageInput.at<float>(x, y+1)*(float)(1.0f/3.0f);
+        
+            imageOutput.at<float>(x, y) = sum;
+        }
+    }
+
+    return imageOutput;
+}
 
 int main(int argc, char**argv) {
     //prendo img in input da riga di comando
@@ -25,17 +40,19 @@ int main(int argc, char**argv) {
     }
 
     int dim = 3;
-
-    Mat imageOutput;
     
     Mat mask = Mat::ones(3, 3, CV_32F)/(dim*dim);
 
+    Mat imageOutput;
     filter2D(imageInput, imageOutput, imageInput.type(), mask);
+    Mat imageOutputMyCorrelation = correlation1D(imageInput);
 
     cv::imshow("Original Image", imageInput);
-    cv::waitKey(0);
     cv::imshow("Filter2d Image", imageOutput);
+    cv::imshow("Custom myCorrelation Image", imageOutputMyCorrelation);
     cv::waitKey(0);
 
     return 0;
 }
+
+//TODO: another file cpp w correlation and convolution
