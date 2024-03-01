81f66a718800fe2af24a177a17e24cdb96fdec82
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2024-03-01 11:32:05 +0100

    test5

---
+++ Canny_Algorithm/test5/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+const int ht = 100;
+const int lt = 50;
+
+void calcMagAng(const Mat& src, Mat& mag, Mat& ang) {
+    Mat dx, dy;
+    Sobel(src, dx, CV_32F, 1, 0);
+    Sobel(src, dy, CV_32F, 0, 1);
+    magnitude(dx, dy, mag);
+    phase(dx, dy, ang, true);
+}
+
+void calcNMS(const Mat& mag, const Mat& ang, Mat& nmsOut) {
+    copyMakeBorder(mag, nmsOut, 1, 1, 1, 1, BORDER_CONSTANT, Scalar(0));
+    for(int y = 1; y < mag.rows; y++) {
+        for(int x = 1; x < mag.cols; x++) {
+            float angVal = ang.at<float>(y-1,x-1) > 180 ? ang.at<float>(y,x)-180 : ang.at<float>(y,x);
+            if(angVal >= 0 && angVal <= 22.5 || angVal >= 157.5 && angVal <= 180) {
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x)) {
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            } else if(angVal > 22.5 && angVal <= 67.5) {
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x-1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x+1)) {
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            } else if(angVal > 67.5 && angVal <= 112.5) {
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y,x-1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y,x+1)) {
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            } else if(angVal > 112.5 && angVal <= 157.5) {
+                if(nmsOut.at<float>(y,x) < nmsOut.at<float>(y-1,x+1) || nmsOut.at<float>(y,x) < nmsOut.at<float>(y+1,x-1)) {
+                    nmsOut.at<float>(y,x) = 0;
+                }
+            }
+        }
+    }
+}
+
+void myThreshold(const Mat& nmsOut, Mat& dest) {
+    dest = Mat::zeros(nmsOut.rows-2, nmsOut.cols-2, CV_8U);
+    for(int y = 0; y < nmsOut.rows-1; y++) {
+        for(int x = 0; x < nmsOut.cols-1; x++) {
+            if(nmsOut.at<float>(y,x) > ht)
+                dest.at<uchar>(y-1, x-1) = 255;
+            else if(nmsOut.at<float>(y, x) < lt)
+                dest.at<uchar>(y-1, x-1) = 0;
+            else {
+                bool strongN = false;
+                for(int j = -1; j >= 1; j++) {
+                    for(int i = -1; i >= 1; i++) {
+                        if(nmsOut.at<float>(y+j, x+i) > ht) {
+                            strongN = true;
+                        }
+                    }
+                }
+                if(strongN)
+                    dest.at<uchar>(y-1, x-1) = 255;
+                else
+                    dest.at<uchar>(y-1, x-1) = 0;
+            }
+        }
+    }
+}
+
+void myCanny(const Mat& src, Mat& dest) {
+    Mat blur, mag, ang, nmsOut;
+    GaussianBlur(src, blur, Size(3,3), 0, 0);
+    calcMagAng(blur, mag, ang);
+    calcNMS(mag, ang, nmsOut);
+    myThreshold(nmsOut, dest);
+}
+
+
+
+int main(int argc, char**argv) {
+    if(argc < 2) {
+        cout << "error" << endl;
+        exit(0);
+    }
+    Mat src = imread(argv[1], IMREAD_GRAYSCALE);
+    if(src.empty()) {
+        cout << "error" << endl;
+        exit(0);
+    }
+    Mat dest, cannyCV;
+    imshow("src", src);
+    myCanny(src, dest);
+    imshow("dest", dest);
+    Canny(src, cannyCV, 30, 100);
+    imshow("cannyCv", cannyCV);
+
+    waitKey(0);
+
+    return 0;
+}
